41004bb5a08c92d5ec4f6d3df28f9390e78eecee
JadyLinnit <linnitjady@gmail.com>
2021-11-08 22:51:24 -0300

    @JadyLinnit [Task - CREATED Get-Post-Put-Delete Categoria]

--- farmacia/src/main/java/org/generation/farmacia/controller/CategoriaController.java
+++ farmacia/src/main/java/org/generation/farmacia/controller/CategoriaController.java
@@ -1,19 +1,27 @@
 package org.generation.farmacia.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.generation.farmacia.model.CategoriaModel;
 import org.generation.farmacia.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 
  * @author marianatheml
+ * @author JadyLinnit
  * @since 1.0
  * 
  *
@@ -27,9 +35,35 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository repository;
 	
-	@GetMapping
+	   @GetMapping
 	public ResponseEntity<List<CategoriaModel>> findAllCategoria () {
 		return ResponseEntity.ok(repository.findAll());
+		}
+	   
+	   @GetMapping("/byId/{id}")
+		public ResponseEntity<Optional<CategoriaModel>> findByIdCategoria (@PathVariable("id") Long id) {
+			return ResponseEntity.ok(repository.findById(id));
+		}
+		
+		@GetMapping("/byDescricao/{descricao}")
+		public ResponseEntity<List<CategoriaModel>> findByDescricaoCategoria (@PathVariable("descricao") String descricao) {
+			return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+		}
+		
+		@PostMapping
+		public ResponseEntity<CategoriaModel> post(@RequestBody CategoriaModel categoria) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+		}
+
+		@PutMapping
+		public ResponseEntity<CategoriaModel> put(@RequestBody CategoriaModel categoria) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+		}
+
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			repository.deleteById(id);
+	
 	}
 	
 }
--- farmacia/src/main/java/org/generation/farmacia/controller/ProdutoController.java
+++ farmacia/src/main/java/org/generation/farmacia/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package org.generation.farmacia.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.generation.farmacia.model.ProdutoModel;
 import org.generation.farmacia.repositories.ProdutoRepository;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,15 @@ public class ProdutoController {
 	@GetMapping
 	public ResponseEntity<List<ProdutoModel>> findAllProduto () {
 		return ResponseEntity.ok(repository.findAll());
+	}
+	@GetMapping("/byId/{id}")
+	public ResponseEntity<Optional<ProdutoModel>> findByIdProduto(@PathVariable("id") Long id) {
+		return ResponseEntity.ok(repository.findById(id));
+	}
+
+	@GetMapping("/byDescricao/{descricao}")
+	public ResponseEntity<List<ProdutoModel>> findByDescricaoProduto(@PathVariable("descricao") String descricao) {
+		return ResponseEntity.ok(repository.findByDescricaoContainingIgnoreCase(descricao));
 	}
 	
 }
